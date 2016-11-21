a4f1f1020a276945b40d469de59634e177f87634
atpons <taiga@atpons.com>
2016-11-21 21:23:31 +0900

    running tested

--- app.rb
+++ app.rb
@@ -1,37 +1,39 @@
 require "docker-api"
 require "sinatra"
 
-Docker.url = "http://127.0.0.1:4243/"
-cons = Docker::Container.all(:running => true)
-cont = Docker::Container.all(:running => true)
-
-
-@CID = []
-@PORT = []
-@IMG = []
-@CNAME = []
-
-
-cons.each do |con|
-  begin
-  @PORT.push(con.json["NetworkSettings"]["Ports"])
-  rescue
-  else
-  end
-@IMG.push(con.json["Config"]["Image"])
-@CID.push(con.json["Id"])
-@CNAME.push(con.json["Name"])
-end
-
-image = Docker::Image.all
-image.each do |img|
-  puts img.json["RepoTags"]
-end
-puts @CID
-puts @IMG
-puts @CNAME
-puts @PORT
+set :bind, '0.0.0.0'
 
 get "/" do
+@image = Docker::Image.all
+Docker.url = "http://127.0.0.1:4243/"
+cons = Docker::Container.all(:running => true)
+@cont = Docker::Container.all(:running => true)
   erb :index
 end
+
+post "/run" do
+ Docker.url = "http://127.0.0.1:4243/"
+ @img = @params[:img]
+ @port = @params[:port]
+@container = Docker::Container.create(
+      'Image' => @img,
+'OpenStdin' => true,
+'ExposedPorts' => { '80/tcp' => {} },
+'Tty' => true,
+      'HostConfig' => { 'Privileged' => true, 'PortBindings' => {
+      '80/tcp' => [{}]
+    }},
+      'Cmd': [ '/bin/bash' ]
+    ) 
+@container.start
+@container.exec(["./run.sh"], detach: true)
+erb :run
+end
+
+get "/stop" do
+  @id = params["id"]
+  @container = Docker::Container.get(@id)
+  @container.stop
+  erb :stop
+end
+
