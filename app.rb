99620b2fe03d4b541b1c651015456fe08732a5e1
atpons <taiga@atpons.com>
2016-11-21 16:08:18 +0900

    saikou

--- app.rb
+++ app.rb
@@ -1,13 +1,31 @@
 require "docker-api"
+require "sinatra"
 
 Docker.url = "http://127.0.0.1:4243/"
 cons = Docker::Container.all(:running => true)
 
+@CID = []
+@PORT = []
+@IMG = []
+@CNAME = []
+
+
 cons.each do |con|
-  puts con.json["Config"]["Image"]
+  begin
+  @PORT.push(con.json["NetworkSettings"]["Ports"])
+  rescue
+  else
+  end
+@IMG.push(con.json["Config"]["Image"])
+@CID.push(con.json["Id"])
+@CNAME.push(con.json["Name"])
 end
 
 image = Docker::Image.all
 image.each do |img|
-  puts img.json
+  puts img.json["RepoTags"]
 end
+puts @CID
+puts @IMG
+puts @CNAME
+puts @PORT
