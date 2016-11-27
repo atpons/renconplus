c4b3e29ebe8af711240201e91f3825e234aa64f1
atpons <taiga@atpons.com>
2016-11-27 21:18:23 +0900

    New feature: Environment or Commands Settings

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require "docker-api"
 require "sinatra"
 require "dotenv"
 
+def empty(str)
+   case str.empty?
+   when true
+       return ""
+   else
+       return str.split(/\s*,\s*/)
+   end
+end
+
 Dotenv.load
 set :environment, :production
 set :bind, '0.0.0.0'
@@ -19,12 +28,12 @@ end
 post "/run" do
   @title = "Run"
   @img = @params[:img]
-  @env = @params[:env].split(",")
-  @cmd = @params[:cmd].split(",")
+  @environment = empty(@params[:environment])
+  @command = empty(@params[:command])
   @container = Docker::Container.create(
     'Image' => @img,
-    'Env' => @env,
-    'Cmd' => @cmd,
+    'Env' => @environment,
+    'Cmd' => @command,
     'ExposedPorts' => { '80/tcp' => {} },
     'HostConfig' => { 'Privileged' => true, 'PortBindings' => {
       '80/tcp' => [{}]
